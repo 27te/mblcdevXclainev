d07b81e1e28f2686bc274a1c72fe6dc1528891f2
Antonio <marcoaranibar12@gmail.com>
2025-05-13 17:32:43 -0500

    refactor: simplify blog post URLs and add diary collection
    - Remove redundant "blog/" prefix from post URLs to streamline links
    - Add new diary collection schema to support additional content types

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,19 @@ const blog = defineCollection({
     tags: z.array(z.string()).default(["uncategorized"]),
   }),
 });
+const diary = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    draft: z.boolean().optional(),
+    image: z.string().optional(),
+    author: z.string().default("Anonymous"),
+    language: z.enum(["en", "es"]),
+  }),
+});
 export const collections = {
   blog,
+  diary,
 };
